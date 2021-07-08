2ae68897aad7cff2094f6feb6e72a10c36aabdd2
s291039 <s291039@studenti.polito.it>
2021-07-08 15:26:16 +0200

    Added some dynamics

--- src/Components/ModalForm.js
+++ src/Components/ModalForm.js
@@ -25,20 +25,54 @@ export default function ModalForm(props) {
 			: ''
 	);
 
-	const images_path = [];
-	for (let i=1; i<=12; i++) {
-		images_path[i] = `${process.env.PUBLIC_URL + '/Images/image' + i + '.jpg'}`;
-	}
+	const images_array = [
+		{
+			image_name: 'image0',
+			image_path: `${process.env.PUBLIC_URL + '/Images/image0.jpg'}`,
+			positions_class: 'positions-image0',
+			n_text_fields: 2
+		},
+		{
+			image_name: 'image1',
+			image_path: `${process.env.PUBLIC_URL + '/Images/image1.jpg'}`,
+			positions_class: 'positions-image1',
+			n_text_fields: 1
+		},
+		{
+			image_name: 'image2',
+			image_path: `${process.env.PUBLIC_URL + '/Images/image2.jpg'}`,
+			positions_class: 'positions-image2',
+			n_text_fields: 4
+		},
+		{
+			image_name: 'image3',
+			image_path: `${process.env.PUBLIC_URL + '/Images/image3.jpg'}`,
+			positions_class: 'positions-image3',
+			n_text_fields: 2
+		},
+		{
+			image_name: 'image4',
+			image_path: `${process.env.PUBLIC_URL + '/Images/image4.jpg'}`,
+			positions_class: 'positions-image4',
+			n_text_fields: 3
+		},
+		{
+			image_name: 'image5',
+			image_path: `${process.env.PUBLIC_URL + '/Images/image5.jpg'}`,
+			positions_class: 'positions-image5',
+			n_text_fields: 2
+		}
+	];
 	
 	// Selected image for meme
-	const [index_current_image, setIndexCurrentImage] = useState(1);
+	const [index_curimg, setIndexCurImg] = useState(0);
 
 	// Text style (font, color, ecc.)
 	const [text_style, setTextStyle] = useState("");
 
 	// Add another text style (ex. bold + color)
 	const addTextStyle = (extra_style) => {
-		setTextStyle(text_style + extra_style);
+		setTextStyle(text_style + ' ' + extra_style);
 	};
 
 
@@ -137,145 +171,289 @@ export default function ModalForm(props) {
 					{location.state ? 'Edit meme' : 'Add a new meme'}
 				</Modal.Title>
 			</Modal.Header>
+			
 			<Modal.Body>
-
-			<Row>
-				<Col lg={{ span: 4, order: 1}} md={{ span: 12, order: 2}}>
-					<section
-						className="top-center bgimg"
-						style={{ backgroundImage: 'url(' + images_path[index_current_image] + ')' }}>
-
-						<div className="text_style">
-							Questa è una prova per il secondo meme...
-						</div>
-
-					</section>
-				</Col>
-
-				<Col lg={{ span: 8, order: 2}} md={{ span: 12, order: 1}}>
-					
-					<Row>
-						<Figure as={Col} lg="2" sm="3" style={{cursor: 'pointer'}}>
-							<Figure.Image
-								src={images_path[1]} thumbnail
-								onClick={() => setIndexCurrentImage(1)}
-								/>
-						</Figure>
-						<Figure as={Col} lg="2" sm="3" style={{cursor: 'pointer'}}>
-							<Figure.Image
-								src={images_path[2]} thumbnail
-								onClick={() => setIndexCurrentImage(2)}
-								/>
-						</Figure>
-						<Figure as={Col} lg="2" sm="3" style={{cursor: 'pointer'}}>
-							<Image
-								src={images_path[3]} thumbnail
-								onClick={() => setIndexCurrentImage(3)}
-								/>
-						</Figure>
-						<Figure as={Col} lg="2" sm="3" style={{cursor: 'pointer'}}>
-							<Image
-								src={images_path[4]} thumbnail
-								onClick={() => setIndexCurrentImage(4)}
-								/>
-						</Figure>
-						<Figure as={Col} lg="2" sm="3" style={{cursor: 'pointer'}}>
-							<Image
-								src={images_path[5]} thumbnail
-								onClick={() => setIndexCurrentImage(5)}
-							/>
-						</Figure>
-						<Figure as={Col} lg="2" sm="3" style={{cursor: 'pointer'}}>
-							<Image
-								src={images_path[6]} thumbnail
-								onClick={() => setIndexCurrentImage(6)}
-							/>
-						</Figure>
-						<Figure as={Col} lg="2" sm="3" style={{cursor: 'pointer'}}>
-							<Image
-								src={images_path[7]} thumbnail
-								onClick={() => setIndexCurrentImage(7)}
-							/>
-						</Figure>
-						<Figure as={Col} lg="2" sm="3" style={{cursor: 'pointer'}}>
-							<Image
-								src={images_path[8]} thumbnail
-								onClick={() => setIndexCurrentImage(8)}
-							/>
-						</Figure>
-						<Figure as={Col} lg="2" sm="3" style={{cursor: 'pointer'}}>
-							<Image
-								src={images_path[9]} thumbnail
-								onClick={() => setIndexCurrentImage(9)}
-								/>
-						</Figure>
-						<Figure as={Col} lg="2" sm="3" style={{cursor: 'pointer'}}>
-							<Image
-								src={images_path[10]} thumbnail
-								onClick={() => setIndexCurrentImage(10)}
-								/>
-						</Figure>
-						<Figure as={Col} lg="2" sm="3" style={{cursor: 'pointer'}}>
-							<Image
-								src={images_path[11]} thumbnail
-								onClick={() => setIndexCurrentImage(11)}
-								/>
-						</Figure>
-						<Figure as={Col} lg="2" sm="3" style={{cursor: 'pointer'}}>
-							<Image
-								src={images_path[12]} thumbnail
-								onClick={() => setIndexCurrentImage(12)}
-								/>
-						</Figure>
-					</Row>
-
+				<Container>
 					<Form noValidate onSubmit={handleConfirmButton}>
+						<Container>
 
-						<Form.Group>
-							<Form.Label>Title</Form.Label>
-							<InputGroup>
-								<Form.Control
-									type="text"
-									placeholder="Meme title"
-									value={description}
-									onChange={(ev) => handleDescription(ev)}
-									required
-									ref={descriptionRef}
-									isValid={validated}
-									isInvalid={validated === undefined ? undefined : !validated}
-								/>
-								<Form.Control.Feedback type="invalid">
-									Please insert a title.
-								</Form.Control.Feedback>
-							</InputGroup>
-						</Form.Group>
-
-						{/* <Form.Group>
-							<Form.Label>Deadline</Form.Label>
-							<Row className="align-items-center">
-								<Col>
-									<Form.Control
-										type="datetime-local"
-										value={deadline}
-										onChange={(ev) => setDeadline(ev.target.value)} // `deadline` will be in the format: 'YYYY-MM-DDTHH:mm'
+							{/* Row 1 : images thumbnails */}
+							<Row>
+								<Figure as={Col} lg="2" xs="4" style={{cursor: 'pointer'}}>
+									<Figure.Image
+										src={images_array[0].image_path} thumbnail
+										onClick={() => setIndexCurImg(0)}
 									/>
-								</Col>
-								<Col xs="auto">
-									<Button
-										variant="outline-dark"
-										size="sm"
-										onClick={() => setDeadline('')}
-										disabled={!deadline}
-									>
-										No deadline
-									</Button>
-								</Col>
+								</Figure>
+								<Figure as={Col} lg="2" xs="4" style={{cursor: 'pointer'}}>
+									<Figure.Image
+										src={images_array[1].image_path} thumbnail
+										onClick={() => setIndexCurImg(1)}
+									/>
+								</Figure>
+								<Figure as={Col} lg="2" xs="4" style={{cursor: 'pointer'}}>
+									<Figure.Image
+										src={images_array[2].image_path} thumbnail
+										onClick={() => setIndexCurImg(2)}
+									/>
+								</Figure>
+								<Figure as={Col} lg="2" xs="4" style={{cursor: 'pointer'}}>
+									<Figure.Image
+										src={images_array[3].image_path} thumbnail
+										onClick={() => setIndexCurImg(3)}
+									/>
+								</Figure>
+								<Figure as={Col} lg="2" xs="4" style={{cursor: 'pointer'}}>
+									<Figure.Image
+										src={images_array[4].image_path} thumbnail
+										onClick={() => setIndexCurImg(4)}
+									/>
+								</Figure>
+								<Figure as={Col} lg="2" xs="4" style={{cursor: 'pointer'}}>
+									<Figure.Image
+										src={images_array[5].image_path} thumbnail
+										onClick={() => setIndexCurImg(5)}
+									/>
+								</Figure>
 							</Row>
-						</Form.Group> */}
 
-						<Form.Group>
-							<Row className="align-items-center">
-								<Col xs="auto">
+							<hr/>
+
+							{/* Row 2: Selected image, meme title and `private` checkbox */}
+							<Row>
+								
+								{/* {Row2, Col1}: Selected image */}
+								<Col xl={4} lg={6} xs={{ span: 11, offset: 1 }} className="ml-lg-0 ml-sm-4">
+									<section
+										className={"bgimg " + images_array[index_curimg].positions_class}
+										style={{ backgroundImage: "url(" + images_array[index_curimg].image_path + ")" }}>
+
+										<div className={text_style}>
+											Questa è una prova per il primo text field...
+										</div>
+
+										<div className={images_array[index_curimg].n_text_fields > 1 ? "" : "d-none"}>
+											Questa è una prova per il secondo text field...
+										</div>
+										
+										<div className={images_array[index_curimg].n_text_fields > 2 ? "" : "d-none"}>
+											Questa è una prova per il terzo text field...
+										</div>
+
+									</section>
+								</Col>
+
+								{/* {Row2, Col2}: Text fields */}
+								<Col xl={4} lg={6} xs={12} className="pt-lg-0 pt-sm-3">
+								
+									{/* <Form.Group>
+										<Form.Label style={{ fontSize: '115%' }}>
+											Text fields
+										</Form.Label>
+									</Form.Group> */}
+
+									{/* Text #1 */}
+									<Form.Group>
+										{/* <Form.Label>Text #1</Form.Label> */}
+										<InputGroup>
+											<Form.Control
+												type="text"
+												placeholder="Text #1"
+												value={description}
+												onChange={(ev) => handleDescription(ev)}
+												required
+												ref={descriptionRef}
+												isValid={validated}
+												isInvalid={validated === undefined ? undefined : !validated}
+											/>
+											<Form.Control.Feedback type="invalid">
+												Please insert a title.
+											</Form.Control.Feedback>
+										</InputGroup>
+									</Form.Group>
+
+									{/* Text #2 */}
+									<Form.Group className={images_array[index_curimg].n_text_fields > 1 ? "" : "d-none"}>
+										{/* <Form.Label>Text #2</Form.Label> */}
+										<InputGroup>
+											<Form.Control
+												type="text"
+												placeholder="Text #2"
+												value={description}
+												onChange={(ev) => handleDescription(ev)}
+												required
+												ref={descriptionRef}
+												isValid={validated}
+												isInvalid={validated === undefined ? undefined : !validated}
+											/>
+											<Form.Control.Feedback type="invalid">
+												Please insert a title.
+											</Form.Control.Feedback>
+										</InputGroup>
+									</Form.Group>
+
+									{/* Text #3 */}
+									<Form.Group className={images_array[index_curimg].n_text_fields > 2 ? "" : "d-none"}>
+										{/* <Form.Label>Text #3</Form.Label> */}
+										<InputGroup>
+											<Form.Control
+												type="text"
+												placeholder="Text #3"
+												value={description}
+												onChange={(ev) => handleDescription(ev)}
+												required
+												ref={descriptionRef}
+												isValid={validated}
+												isInvalid={validated === undefined ? undefined : !validated}
+											/>
+											<Form.Control.Feedback type="invalid">
+												Please insert a title.
+											</Form.Control.Feedback>
+										</InputGroup>
+									</Form.Group>
+
+								</Col>
+
+								{/* {Row2, Col3}: Text styles */}
+								<Col xl={4} lg={12} xs={12} className="pt-xl-0 pt-lg-3">
+									
+									{/* <Form.Group>
+										<Form.Label style={{ fontSize: '115%', textIndent: '3em' }}>
+											Text styles
+										</Form.Label>
+									</Form.Group> */}
+
+									<Form.Group>
+										<Form.Row>
+											<Form.Label column="sm" xs={2}>
+												Font
+											</Form.Label>
+											<Col xs={10} className="pl-0">
+												<Form.Control
+													size="sm"
+													type="text"
+													as="select"
+													id="inlineFormCustomSelect"
+													custom
+												>
+													<option value="1">Impact</option>
+													<option value="2">Arial</option>
+													<option value="3">Georgia</option>
+												</Form.Control>
+											</Col>
+										</Form.Row>
+									</Form.Group>
+
+									<Form.Group>
+										<Form.Row>
+											<Form.Label column="sm" xs={2}>
+												Color
+											</Form.Label>
+											<Col xs={10} className="pl-0">
+												<Form.Control
+													size="sm"
+													type="text"
+													as="select"
+													id="inlineFormCustomSelect"
+													custom
+												>
+													<option value="1">Black</option>
+													<option value="2">White</option>
+													<option value="3">Red</option>
+												</Form.Control>
+											</Col>
+										</Form.Row>
+									</Form.Group>
+
+									<Form.Group>
+										<Row>
+											<Col xs={{ span: 4, offset: 2 }} className="font-weight-bolder pl-2">
+												<Form.Check
+													custom
+													inline
+													type="checkbox"
+													label="Bold"
+													id="form_bold"
+													checked={is_private}
+													onChange={() => setIsPrivate((isp) => !isp)}
+												/>
+											</Col>
+											<Col xs={{ span: 4, offset: 2 }} className="font-italic pl-4">
+												<Form.Check
+													custom
+													inline
+													type="checkbox"
+													label="Italic"
+													id="form_italic"
+													checked={is_private}
+													onChange={() => setIsPrivate((isp) => !isp)}
+												/>
+											</Col>
+										</Row>
+									</Form.Group>
+
+									<Form.Group>
+										<Row>
+											<Col xs={{ span: 4, offset: 2 }} className="pl-2">
+												<Form.Check
+													custom
+													inline
+													type="checkbox"
+													label="ALLCAPS"
+													id="form_italic"
+													checked={is_private}
+													onChange={() => setIsPrivate((isp) => !isp)}
+												/>
+											</Col>
+											<Col xs={{ span: 4, offset: 2 }} className="text-monospace pl-4">
+												<Form.Check
+													custom
+													inline
+													type="checkbox"
+													label="Mono"
+													id="form_bold"
+													checked={is_private}
+													onChange={() => setIsPrivate((isp) => !isp)}
+												/>
+											</Col>
+										</Row>
+									</Form.Group>
+
+								</Col>
+
+							</Row>
+
+							<hr/>
+
+							{/* Row 3: Meme title and private checkbox */}
+							<Row>
+
+								{/* Meme title (label) */}
+								<Col lg={4} xs={12}>
+									<Form.Label className="pt-2" style={{ fontSize: '105%' }}>
+										Remember to give it an epic title...
+									</Form.Label>
+								</Col>
+
+								{/* Meme title (form) */}
+								<Col lg={4} xs={12}>
+									<Form.Control
+										type="text"
+										placeholder="Meme title"
+										value={description}
+										onChange={(ev) => handleDescription(ev)}
+										required
+										ref={descriptionRef}
+										isValid={validated}
+										isInvalid={validated === undefined ? undefined : !validated}
+									/>
+									<Form.Control.Feedback type="invalid">
+										Please insert a title.
+									</Form.Control.Feedback>
+								</Col>
+								
+								{/* Private checkbox */}
+								<Col lg={4} xs={12} className="pt-2">
 									<Form.Check
 										custom
 										inline
@@ -286,19 +464,20 @@ export default function ModalForm(props) {
 										onChange={() => setIsPrivate((isp) => !isp)}
 									/>
 								</Col>
+
 							</Row>
-						</Form.Group>
+
+						</Container>
 					</Form>
-					
-				</Col>
-			</Row>
+				</Container>
 
 			</Modal.Body>
+
 			<Modal.Footer>
 				<Button variant="secondary" onClick={handleCancelButton}>
 					Cancel
 				</Button>
-				<Button variant="primary" onClick={handleConfirmButton}>
+				<Button variant="primary" onClick={handleConfirmButton} className="mr-5">
 					Confirm
 				</Button>
 			</Modal.Footer>
